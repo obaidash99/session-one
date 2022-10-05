80ae6f888e650f05dceb31ca59ebe7d901e5f294
obaida <obaidashurbaji99@gmail.com>
2022-10-05 23:22:59 +0200

    following up 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 
 function App() {
+	const myName = 'Obaida';
+	function greeting(name) {
+		const profileLink = 'https://google.com';
+		if (name) {
+			return (
+				<h2 className="name-title">
+					Hello, <a href={profileLink}>{name}</a>
+				</h2>
+			);
+		} else {
+			return <h2>Hello, Guest</h2>;
+		}
+	}
 	return (
-		<div className="App">
-			<h1>Hello React App</h1>
-		</div>
+		<>
+			<div className="App">
+				<h1>Hello React App</h1>
+				{greeting(myName)}
+			</div>
+			<div className="terst">Teeessstttt</div>
+		</>
 	);
 }
 
